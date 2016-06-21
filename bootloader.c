978d19025cd6f2dec2af09a9bd9049d4d023479b
Sean Cross <sean@xobs.io>
2016-06-21 17:45:24 +0800

    bootloader: add token support, and make booting work
    Signed-off-by: Sean Cross <sean@xobs.io>

--- bootloader.c
+++ bootloader.c
@@ -1,9 +1,26 @@
 #include <stdint.h>
+#include "kl17.h"
+#include "core_cm0plus.h"
 #include "palawan.h"
 #include "radio.h"
 
 int updateRx(void);
 int updateTx(void);
+
+extern uint32_t boot_token;
+static __attribute__ ((section(".appvectors"))) uint32_t appVectors[64];
+
+static int test_boot_token()
+{
+  /*
+   * If we find a valid boot token in RAM, the application is asking us explicitly
+   * to enter DFU mode. This is used to implement the DFU_DETACH command when the app
+   * is running.
+   */
+
+  return boot_token == 0x74624346;
+}
+
 
 static int should_enter_bootloader(void) {
   return 1;
@@ -11,6 +28,23 @@ static int should_enter_bootloader(void) {
 
 __attribute__((noreturn))
 static void boot_app(void) {
+  // Relocate IVT to application flash
+  __disable_irq();
+  SCB->VTOR = (uint32_t) &appVectors[0];
+
+  // Refresh watchdog right before launching app
+  watchdog_refresh();
+
+  // Clear the boot token, so we don't repeatedly enter DFU mode.
+  boot_token = 0;
+
+  asm volatile (
+  "mov lr, %0 \n\t"
+  "mov sp, %1 \n\t"
+  "bx %2 \n\t"
+  : : "r" (0xFFFFFFFF),
+  "r" (appVectors[0]),
+  "r" (appVectors[1]) );
   while (1)
     ;
 }
@@ -18,16 +52,15 @@ static void boot_app(void) {
 __attribute__((noreturn))
 void bootloader_main(void) {
 
-  if (!should_enter_bootloader())
-    boot_app();
+  if (should_enter_bootloader()) {
 
-  if (palawanModel() == palawan_rx)
-    /* Start USB */
-    updateRx();
+    if (palawanModel() == palawan_rx)
+      /* Start USB */
+      updateRx();
 
-  else if (palawanModel() == palawan_tx) {
-    updateTx();
+    else if (palawanModel() == palawan_tx)
+      updateTx();
   }
-  while(1)
-    ;
+
+  boot_app();
 }
--- kl17.h
+++ kl17.h
@@ -1232,4 +1232,47 @@ typedef struct
 #define FTFx_FPROT3 0x40020010
 #define FTFx_FDPROT 0x40020017
 
+
+// Chapter 23: Watchdog Timer (WDOG)
+#define WDOG_STCTRLH            *(volatile uint16_t *)0x40052000 // Watchdog Status and Control Register High
+#define WDOG_STCTRLH_DISTESTWDOG    (uint16_t)0x4000        // Allows the WDOG's functional test mode to be disabled permanently.
+#define WDOG_STCTRLH_BYTESEL(n)     (uint16_t)(((n) & 3) << 12) // selects the byte to be tested when the watchdog is in the byte test mode.
+#define WDOG_STCTRLH_TESTSEL        (uint16_t)0x0800
+#define WDOG_STCTRLH_TESTWDOG       (uint16_t)0x0400
+#define WDOG_STCTRLH_WAITEN     (uint16_t)0x0080
+#define WDOG_STCTRLH_STOPEN     (uint16_t)0x0040
+#define WDOG_STCTRLH_DBGEN      (uint16_t)0x0020
+#define WDOG_STCTRLH_ALLOWUPDATE    (uint16_t)0x0010
+#define WDOG_STCTRLH_WINEN      (uint16_t)0x0008
+#define WDOG_STCTRLH_IRQRSTEN       (uint16_t)0x0004
+#define WDOG_STCTRLH_CLKSRC     (uint16_t)0x0002
+#define WDOG_STCTRLH_WDOGEN     (uint16_t)0x0001
+#define WDOG_STCTRLL            *(volatile uint16_t *)0x40052002 // Watchdog Status and Control Register Low
+#define WDOG_TOVALH             *(volatile uint16_t *)0x40052004 // Watchdog Time-out Value Register High
+#define WDOG_TOVALL             *(volatile uint16_t *)0x40052006 // Watchdog Time-out Value Register Low
+#define WDOG_WINH               *(volatile uint16_t *)0x40052008 // Watchdog Window Register High
+#define WDOG_WINL               *(volatile uint16_t *)0x4005200A // Watchdog Window Register Low
+#define WDOG_REFRESH            *(volatile uint16_t *)0x4005200C // Watchdog Refresh register
+#define WDOG_UNLOCK             *(volatile uint16_t *)0x4005200E // Watchdog Unlock register
+#define WDOG_UNLOCK_SEQ1        (uint16_t)0xC520
+#define WDOG_UNLOCK_SEQ2        (uint16_t)0xD928
+#define WDOG_TMROUTH            *(volatile uint16_t *)0x40052010 // Watchdog Timer Output Register High
+#define WDOG_TMROUTL            *(volatile uint16_t *)0x40052012 // Watchdog Timer Output Register Low
+#define WDOG_RSTCNT             *(volatile uint16_t *)0x40052014 // Watchdog Reset Count register
+#define WDOG_PRESC *(volatile uint16_t *)0x40052016 // Watchdog Prescaler register
+
+static inline void watchdog_refresh(void)
+{
+  __disable_irq();
+  WDOG_REFRESH = 0xA602;
+  WDOG_REFRESH = 0xB480;
+  __enable_irq();
+}
+
+static inline void watchdog_reboot(void)
+{
+  // Any invalid write to the WDOG registers will trigger an immediate reboot
+  WDOG_REFRESH = 0;
+}
+
 #endif /* __KL17_H__ */
